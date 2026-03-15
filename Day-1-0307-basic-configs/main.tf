93ecf480ad907613829cc57211490e4f837ab986
sathya <sathya.filenet@gmail.com>
2026-03-15 00:39:38 -0400

    Chanegs committed

--- Day-1-0307-basic-configs/main.tf
+++ Day-1-0307-basic-configs/main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "name" {
     }
   
 }
+
+resource "aws_instance" "name" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "sathya-instance"
+    }
+  
+}
